54fbf9480c403569c887c7f4afcca60fb88ec986
SoSlow <godisfool@gmail.com>
2013-04-04 00:59:35 +0400

    Minor changes

--- main.cpp
+++ main.cpp
@@ -25,11 +25,24 @@ struct TriangleFuzzySet
 
 		return 0 ;		
 	}
+
+	double Norm()
+	{
+		const int N = 1000;
+		double h = (c - a)/N;
+		double s = 0;
+		for(double x = a; x <= c; x += h)
+		{
+			s += MembershipVal(x)*h;
+		}
+
+		return s;
+	}
 };
 
 //////////////////////////////////////
 //@brief
-void LoadInputSets(std::vector<TriangleFuzzySet> &A, char *fname = "in.txt")
+void LoadInputSets(std::vector<TriangleFuzzySet> &A, const char *fname = "in.txt")
 {
 	FILE *f;
 	int N;
@@ -59,13 +72,8 @@ double Hemming(TriangleFuzzySet &A, TriangleFuzzySet &B)
         //double t = abs(A.MembershipVal(x) - B.MembershipVal(x));
 		s += abs(A.MembershipVal(x) - B.MembershipVal(x))*h;
     }
-	double l = (x_max - x_min);
-
-	if(A.c <= B.a)
-		l -= B.a - A.c;
-	if(A.a >= B.c)
-		l -= A.a - B.c;
-
+	double l = A.Norm() + B.Norm();
+	
     return s/l;
 }
 
@@ -104,9 +112,10 @@ std::vector<std::vector<double> > HemmingMatrix(std::vector<TriangleFuzzySet> &A
 std::vector<std::vector<double> > LoadInputMatrix(/*std::vector<std::vector<double> > &in*/)
 {
     std::vector<std::vector<double> > in;
-    //Чтение из файла исходной информации
+    char *fname = "in";
+	//Чтение из файла исходной информации
     int size = 0;
-    FILE *f = fopen("in.txt", "r+");
+    FILE *f = fopen(fname, "r+");
     fscanf(f, "%d", &size);
     in.resize(size);
     for(int i = 0; i < size; ++i)
@@ -117,6 +126,8 @@ std::vector<std::vector<double> > LoadInputMatrix(/*std::vector<std::vector<doub
             fscanf(f, "%lf", &in[i][j]);
         }
     }
+
+	fclose(f);
     return in;
 }
 
@@ -126,7 +137,7 @@ void PrintMatrix(std::vector<std::vector<double> > &in, const char *str){
     {
         for(int j = 0; j < (int) in.size(); ++j)
         {
-            printf("%lf ", in[i][j]);
+            printf("%1.3lf\t", in[i][j]);
         }
         printf("\n");
     }
@@ -216,11 +227,12 @@ std::vector<std::vector<double> > MinMaxComposition(const std::vector<std::vecto
 //@brief compare of two double valued symetric matrix
 bool VecCmp(const std::vector<std::vector<double> > &R1, const std::vector<std::vector<double> > &R2)
 {
+	const double eps = 1e-4;
 	bool flag = true;
 
 	for(int i = 0; i < R1.size(); ++i)
 		for(int j = 0; j < R1.size(); ++j)
-			if(R1[i][j] != R2[i][j]) 
+			if(std::abs(R1[i][j]-R2[i][j]) > eps) 
 			{
 				flag = false;
 				break;
@@ -234,14 +246,18 @@ std::vector<std::vector<double> > MinMaxTransitiveClosure(const std::vector<std:
 {
 	int N = R.size();
 	std::vector<std::vector<double> > R_TrCl_1(N), R_TrCl(N);
-
+	int i = 1;
 	R_TrCl = R;
 	do
 	{
 		R_TrCl_1 = R_TrCl;
 		R_TrCl = MinMaxComposition(R_TrCl, R);
+		char title[255];
+		sprintf(title, "\nR^%d\n", ++i);
+		PrintMatrix(R_TrCl, title);
+
 	}
-	while(VecCmp(R_TrCl, R_TrCl_1));
+	while(!VecCmp(R_TrCl, R_TrCl_1));
 	
 	return R_TrCl;
 }
@@ -299,7 +315,7 @@ void BlockMatrix(std::vector<std::vector<int> > &R, std::vector<int> &tr)
 		if(R[row][i0] == 0)
 		{
 			i1 = i0;
-			while((R[row][++i1] != 1) && (i1 < N-1));
+			while((i1 < N-1) && (R[row][++i1] != 1));
 
 			if(R[row][i1] == 1)
 			{
@@ -333,17 +349,19 @@ void PrintClasses(std::vector<std::vector<int> > &R, std::vector<int> &tr)
 		printf("}\n");
 		i = k;
 	}
-		
 }
 
 //////////////////////////////////////
-//@brief
+//@brief                                                                                                                                                                                         100
 double GetNextMin(std::vector<std::vector<double> > &R, double cur_min)
 {
+	const double eps = 1e-9;
 	double min = 1;
+	if(cur_min == 1) return 2;
+
 	for(int i = 0; i < R.size(); ++i)
 		for(int j = 0; j <= i; ++j)
-			if(R[i][j] > cur_min && R[i][j] < min)
+			if((R[i][j] > cur_min + eps) && (R[i][j] < min))
 				min = R[i][j];
 	return min;
 }
@@ -355,7 +373,7 @@ void FuzzyClassification(std::vector<std::vector<double> > &R)
 	std::vector<int> tr;
 	std::vector<std::vector<int> > Ra;
 	double alpha = 0;
-	for(alpha = 0.5; alpha <= 0.9; alpha += 0.02)
+	for(alpha = GetNextMin(R, alpha); alpha <= 1; alpha  = GetNextMin(R, alpha))
 	{
 		Ra = AlphaLevel(R, alpha);
 		printf("\n\talpha = %lf", alpha);
@@ -371,21 +389,43 @@ void FuzzyClassification(std::vector<std::vector<double> > &R)
 //@brief main
 int main (void)
 {
-    std::vector<TriangleFuzzySet> A;
+    std::vector<std::vector<double> > in;
+	std::vector<TriangleFuzzySet> A;
     std::vector<std::vector<double> > R;
 	std::vector<std::vector<double> > R_cl;
-
-    LoadInputSets(A);
-
-    R = HemmingMatrix(A);
-    PrintMatrix(R, "\nMatrix R:\n");
+	int i;
+    
+	do
+	{
+		
+		printf("Choose:\n1. 8 elemens finite fuzzy set.\n2. Triangular fuzzy sets.\n");
+		scanf("%d", &i);
+		switch(i)
+		{
+			case 1:
+				in = LoadInputMatrix();
+				R = HemmingMatrix(in);
+				break;
+			case 2:
+				LoadInputSets(A);
+				R = HemmingMatrix(A);
+				break;
+			default: 
+				printf("Wrong value\n");
+		}
+	}while(i != 1 && i !=2);
+ 	
+	PrintMatrix(R, "\nMatrix R:\n");
 
 	R_cl = MinMaxTransitiveClosure(R);
-	PrintMatrix(R_cl, "\nMatrix transitive closure of R:\n");
+	PrintMatrix(R_cl, "\Transitive closure of R:\n");
 
 	RelationNegation(R_cl);
+	PrintMatrix(R_cl, "\nEquality relation :\n");
 
 	FuzzyClassification(R_cl);
+
+	getchar();
 
 	getchar();
     return 0;
